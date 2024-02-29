58118d64e056532a3f51da2a948ca3ac582fd726
CodeCraftmining <ht2686047@gmail.com>
2024-02-29 22:25:46 +0530

    add script file

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 2;
+console.log("a = ", a, " & b =", b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
