bc9925bef700e63054843c9b627a38eef9613219
CodeCraftmining <ht2686047@gmail.com>
2024-02-29 22:44:33 +0530

    add a unary operator

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ console.log("a - b = ", a - b);
 console.log("a * b = ", a * b);
 console.log("a / b = ", a / b);
 console.log("a % b = ", a % b);
+//unary operator
+let a = 5;
+let b = 2;
+console.log("a = ", a,"& b = ",b);
+a = a + 1//6
+console.log("a = ", a);//6
