3eb23445e451a12027464577bfef861fc945d4fe
CodeCraftmining <ht2686047@gmail.com>
2024-03-01 21:19:41 +0530

    add comparisonn operators

--- script.js
+++ script.js
@@ -17,3 +17,10 @@ let a = 5;
 let b = 2;
 a += 4;//a = a+4
 console.log("a =", a);//9
+//comparison operators
+let a = 5;
+let b = 2;
+console.log("5 != 2", a != b);//true
+let a = 5; //number
+let b = "5"; //string 
+console.log("a == b", a == b);//true
