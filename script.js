0578263779972f2693a73ae68c2549d19d367565
CodeCraftmining <ht2686047@gmail.com>
2024-03-02 17:51:26 +0530

    change the mode

--- script.js
+++ script.js
@@ -53,3 +53,16 @@ if (age < 18){
     console.log("you can not vote");
 
 }
+
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}
+
+if  (mode === "light") {
+    color = "white";
+}
+
+console.log(color);
