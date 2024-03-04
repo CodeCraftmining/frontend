94d6bef1c5ef5d41074f2ea87e56f862612908bd
CodeCraftmining <ht2686047@gmail.com>
2024-03-04 15:36:48 +0530

    used for loop execute 1000 times

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-for (let count = 1; count <= 3; count++) {
-   console.log("how are you harsh tripathi");//3execute
+for (let count = 1; count <= 1000; count++) {
+   console.log("how are you harsh tripathi");//1000execute
 }
 
 console.log("loop has ended");
