a28746b448a8643ea2a08d6e8768eb8a379eaf8d
CodeCraftmining <ht2686047@gmail.com>
2024-03-04 15:29:14 +0530

    add a file  for loop

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-//practice Qs2
-
-let score = prompt("enter your score (0-100):");
-let grade; 
-
-
-if(score >= 90 && score <=100)  {
-   grade = "A";
-}  else if (score >= 70 && score <= 89) {
-   grade = "B";
-}else if (score >= 60 && score <= 69) {
-   grade = "C";
-} else if (score >= 50 && score <= 59){
-   grade = "D";
-}else if (score >= 0 && score <= 49) {
-   grade = "F";
+for (let count = 1; count <= 5; count++) {
+   console.log("apna college");//5execute
 }
 
-console.log("according to your scores, your grade was :", grade)
+console.log("loop has ended");
