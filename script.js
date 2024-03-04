3fd788bdc617eefc791aab679b9ee20ef18ce0eb
CodeCraftmining <ht2686047@gmail.com>
2024-03-04 11:14:37 +0530

    modify the statement

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
- let num = prompt("eneter a number:");
+//practice Qs2
 
- if (num % 3 === 0) {
-    console.log(num, "is Not a multiple of 3");
- }  else {
+let score = 75;
+let grade;
 
-    console.log(num, "is Not a multiple of 3");
- }
+
+if(score >= 90 && score <=100)  {
+   grade = "A";
+}  else if (score >= 70 && score <= 89) {
+   grade = "B";
+}else if (score >= 60 && score <= 69) {
+   grade = "C";
+} else if (score >= 50 && score <= 59){
+   grade = "D";
+}else if (score >= 0 && score <= 49) {
+   grade = "F";
+}
+
+console.log("according to your scores, your grade was :", grade)
