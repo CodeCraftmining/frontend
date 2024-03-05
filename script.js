2203234045ea8d11e25409202811a4726b816b5e
CodeCraftmining <ht2686047@gmail.com>
2024-03-05 21:24:35 +0530

    print 1 to 5

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//calculate sum of 1 to n
-let  sum = 0;
-let n = 100;
-for(let i = 1; i <= 5; i++)  {
-   sum = sum + i;
+for (let i = 1; i <= 5; i++) {
+   console.log("i =", i);
 }
-console.log("sum = ", sum);
-console.log("loop has ended");
