85040012c2b7b6e19bcc5578aee4fa46935806d5
CodeCraftmining <ht2686047@gmail.com>
2024-03-06 16:04:38 +0530

    used do while loop

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let i = 1;
-while (i <= 50){
-console.log("How Are You Harsh");
-i++;
-}
+// let i = 20;
+// while (i <= 10){
+// console.log("How Are You Harsh");
+// i++;
+// }
+
+
+let i = 20;
+do {
+   console.log("harsh tripathi");
+   i++
+} while (i <= 10);
