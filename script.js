d6945c5b998227be991f23f07a402c7b05593025
CodeCraftmining <ht2686047@gmail.com>
2024-03-07 21:50:30 +0530

    print the odd no.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //practice Qs1
 for(let num=0; num<= 100; num++)  {
-   if(num%2 === 0)  {
-      //even number
+   if(num % 2 !== 0)  {
+      //odd number
       console.log("num =", num);
    }
 }
