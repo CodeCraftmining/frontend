c13c7251533b2f57fb459e529dc91611f1b89a37
CodeCraftmining <ht2686047@gmail.com>
2024-03-09 11:04:57 +0530

    find the price

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let obj = {
+    item: "pen",
+    price: 10,
+};
+
+console.log("the cost of", obj.item, "is", obj.price)
+
+
+
 let specialString = 'This is a template literal';
 
 console.log(typeof specialString);
