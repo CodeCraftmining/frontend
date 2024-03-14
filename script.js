8ac788f6751b7264fb117003e3e585a13a8372bf
CodeCraftmining <ht2686047@gmail.com>
2024-03-15 00:35:21 +0530

    find the offer

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let item = [250, 645, 300, 900, 50]
 
-let idex = 0;
+let i = 0;
 for (let val of item){
-    console.log('value at index ${idex} = ${val}');
-    idex++;
+    console.log('value at index ${i} = ${val}');
+    let offer = val / - offer;
+    console.log('value after offer = ${val}');
+    i++;
 }
+
+
