0fb2a293957ea98e616fa8ce22a2ca625b2b45b7
CodeCraftmining <ht2686047@gmail.com>
2024-03-14 23:16:44 +0530

    used the for loop

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let heroes = ["ironman", "thor", "hulk", "spiderman", "antman"];
 for(let idx=0; i < heroes.length; i++){
     console.log(heroes[i]);
 }
+
+
+for (let hero of heroes) {
+    console.log(hero);
+}
