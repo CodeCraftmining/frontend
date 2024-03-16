8548d814388841d10a47f80b2f69fd74a847bea9
CodeCraftmining <ht2686047@gmail.com>
2024-03-16 14:45:43 +0530

    used the for loop the offer

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 let item = [250, 645, 300, 900, 50]
 
-let i = 0;
-for (let val of item)
-    {
-let offer = val / 10;
-item[i] = item[i] - offer;
-console.log('value after offer = ${items[i]}');
-i++
+for (let i = 0; i < item.length; i++)  {
+    let offer = item[i] / 10;
+    items[i] -= offer;
+}
 
-    }
-
-
+console.log(items);
