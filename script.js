b32f056b39884638b6e94e43ea5b45b70de02eb6
CodeCraftmining <ht2686047@gmail.com>
2024-03-16 13:25:24 +0530

    show the ammount of offers

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let item = [250, 645, 300, 900, 50]
 let i = 0;
 for (let val of item)
     {
-console.log('value at index ${i} = ${val}');
 let offer = val / 10;
 item[i] = item[i] - offer;
 console.log('value after offer = ${items[i]}');
