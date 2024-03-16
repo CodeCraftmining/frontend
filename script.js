06023f790b3f9fa719d6f2cbe8fa7cb1fadb3a8e
CodeCraftmining <ht2686047@gmail.com>
2024-03-16 13:23:33 +0530

    items of the offer

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for (let val of item)
 console.log('value at index ${i} = ${val}');
 let offer = val / 10;
 item[i] = item[i] - offer;
-console.log('value after offer = ${val}');
+console.log('value after offer = ${items[i]}');
 i++
 
     }
