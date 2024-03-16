e3f2d836905f66bb8e25a3905b726cc9e568a6f1
CodeCraftmining <ht2686047@gmail.com>
2024-03-16 14:55:20 +0530

    this is food item

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-let item = [250, 645, 300, 900, 50]
-
-for (let i = 0; i < item.length; i++)  {
-    let offer = item[i] / 10;
-    items[i] -= offer;
-}
-
-console.log(items);
+let foodItem = ["potato", "apple", "tomato"];
