24aeb71dc638ae8f8d838e715f9f1d8dc10a4fb2
CodeCraftmining <ht2686047@gmail.com>
2024-03-17 11:09:31 +0530

    find the multiles heroes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let marvel_heroes = ["thor", "spiderman", "ironman"];
 let dcHeroes = ["superman", "batman"];
+let indianHeroes = ["shaktiman", "krish"];
 
-marvel_heroes.concat(dcHeroes);
+marvel_heroes.concat(dcHeroes, indianHeroes);
 console.log(heroes);
