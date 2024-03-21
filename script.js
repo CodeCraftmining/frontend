c2df008c16c416156f3b0dfe25181e9b786f4d58
CodeCraftmining <ht2686047@gmail.com>
2024-03-21 11:27:59 +0530

    this is same function call

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function myfunction() {
     console.log("welcome to apna college!");
     console.log("we are learning js :");
+    console.log("welcome to apna college!");
+    console.log("we are learning js :");
 }
 
-myfunction();
-myfunction();
-myfunction();
 myfunction();
 myfunction();
