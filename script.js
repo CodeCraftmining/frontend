f151f6abd914ef91f7694e82f8c009e39e20d6f7
CodeCraftmining <ht2686047@gmail.com>
2024-03-23 16:35:11 +0530

    add the two functions

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-//function-> 2 numbers, sum
-
-function sum(x, y){
-    //local variable -> scope
-    s = x + y;
-    console.log("x");
-    return s;
+//sum function
+function sum(a, b) {
+    return a + b;
 }
 
-let val = sum(3, 4);
-console.log(x);
-console.log(val);
+const arrowSum = (a, b) => {
+    console.log(a + b);
+};
+
+//multiplication function 
+function mul(a, b) {
+    return a * b;
+}
