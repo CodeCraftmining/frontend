160bc946bcc4a31b47d95a2aa5460ef014a777aa
CodeCraftmining <ht2686047@gmail.com>
2024-03-23 16:40:29 +0530

    used the arrow function call

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ function sum(a, b) {
     return a + b;
 }
 
-const x = 5;
-
-const arrowSum = (a, b) => {
-    console.log(a + b);
-};
-
 //multiplication function 
 function mul(a, b) {
     return a * b;
 }
+
+const arrowMul = (a, b) => {
+    console.log(a * b);
+};
