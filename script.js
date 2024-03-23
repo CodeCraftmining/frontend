e02f4bcb6dc161949c8f836522b0f01d6fe8dcdc
CodeCraftmining <ht2686047@gmail.com>
2024-03-23 16:50:05 +0530

    print the arrowmul rule

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-//sum function
-function sum(a, b) {
-    return a + b;
-}
-
-//multiplication function 
-function mul(a, b) {
-    return a * b;
-}
-
 let arrowMul = (a, b) => {
     return a * b;
+
 };
 
-arrowMul = 5;
+const printHello = () => console.log("hello");
