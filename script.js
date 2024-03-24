c1b94511e809c134251a754f02c03e1b5110edb3
CodeCraftmining <ht2686047@gmail.com>
2024-03-24 10:49:07 +0530

    compare the vowel

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function countVowels(str) {
+    let count = 0;
     for (const char of str) {
-        console.log(char);
+     if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+        count++;
+     }
     }
+    console.log(count);
 }
