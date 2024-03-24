f3c01ff91f9f515c9729633104a9fc7d4c1e8087
CodeCraftmining <ht2686047@gmail.com>
2024-03-24 10:56:43 +0530

    count the vowels

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function countVowels(str) {
     let count = 0;
     for (const char of str) {
+        console.log(char);
      if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
         count++;
      }
     }
-    console.log(count);
+return count;
 }
+
+const countVow = (str) => {
+    
+        let count = 0;
+        for (const char of str) {
+            console.log(char);
+         if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+            count++;
+         }
+        }
+        console.log(count);
+    };
