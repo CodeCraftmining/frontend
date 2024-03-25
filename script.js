09b47f58dc27c5d3f39fe02694c93c511d349d2a
CodeCraftmining <ht2686047@gmail.com>
2024-03-25 10:02:08 +0530

    find  the square of the no.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let arr = ["pune", "delhi", "mumbai"];
-
-arr.forEach((val) => {
-    console.log(val.toUpperCase(), idx, arr);
+let num = [1, 2, 3, 4, 5, 6];
+ 
+num.forEach((num) => {
+    console.log(num * num);
 });
