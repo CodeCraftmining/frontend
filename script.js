7b150316411da6e9336a615455e27cc9c5dce174
CodeCraftmining <ht2686047@gmail.com>
2024-03-27 21:51:30 +0530

    find the gretest no

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-console.log("hello!");
-alert("harsh tripathi");
+let arr = [5, 6, 2, 1, 3];
+const output = arr.reduce((prev, curr)  => {
+    return prev > curr ? prev : curr;
+
+});
+console.log(ouput);
