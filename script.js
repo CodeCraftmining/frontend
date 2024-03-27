3a8093cbc4ac4f74ab2ec53a9131832d084f576e
CodeCraftmining <ht2686047@gmail.com>
2024-03-27 22:16:17 +0530

    find the factorial

--- script.js
+++ script.js
@@ -13,4 +13,11 @@ let sum = arr.reduce((res, curr) =>  {
   return res + curr;
 });
 
-console.log(sum);
+console.log("sum =",sum);
+
+let factorial = arr.reduce((res, curr) => {
+    return res * curr;
+});
+
+
+console.log("factorial =",factorial);
